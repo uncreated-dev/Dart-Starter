839105572b8d3a655b30c65cd9ff3f9118ae6857
Dart_Beginner <esomojumiemmanuel7@gmail.com>
2025-01-02 20:53:19 +0100

    Create 2_basic_exercise.dart
    2. Arithmetic Operations
    Declare two variables a and b with values 5 and 3, respectively. Calculate and print their sum, difference, product, and quotient.

---
+++ 2_basic_exercise.dart
@@ -0,0 +1,13 @@
+void main ( ) {
+
+ int a = 5;
+ int b = 3;
+ {
+  print ('sum of numbers : ${ a + b}' );
+  print ('difference of numbers : ${ a - b}');
+  print ('product of numbers : ${ a * b }') ;
+  print ('quotient of numbers : ${ a ~/ b }' ); 
+ }
+}
+
+  
