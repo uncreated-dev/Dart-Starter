0b307de90a42f935c8f50f9821ec1f1469889957
Dart_Beginner <esomojumiemmanuel7@gmail.com>
2025-01-02 21:39:31 +0100

    5_basic_exercise.dart
    5. Displaying Variables
    Declare two variables firstName and lastName. Assign values and print them to the console using both print() and stdout.writeln().

---
+++ 5_basic_exercise.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  String firstName = 'Jack';
+  String lastName = 'Sparrow';
+
+  print('First name: $firstName');
+  print('Last name: $lastName');
+
+  stdout.writeln('First name: $firstName');
+  stdout.writeln('Last name: $lastName');
+}
