932b146f66fb228018e05937f13414bfa133afe2
Dart_Beginner <esomojumiemmanuel7@gmail.com>
2025-01-02 21:08:10 +0100

    3_basic_exercise.dart
    4. Basic Data Types
    Declare variables for different data types: int, double, String, bool. Assign values and print them to the console.

---
+++ 3_basic_exercise.dart
@@ -0,0 +1,14 @@
+ void main() {
+ 
+  int age = 27;
+  double universityGrade = 3.5;
+  String colour = 'grey';
+  bool sheIsBrilliant = true;
+  {
+   print (age);
+   print (universityGrade);
+   print  (colour);
+   print  (sheIsBrilliant);
+  }
+
+ } 
